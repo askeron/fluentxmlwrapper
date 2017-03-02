cb4f6154834f5a9afea7c0cfc97c730c4d88865b
bkuhlen <bjoern.kuhlen@dumont.de>
2017-03-02 15:06:45 +0100

    added empty tests

---
+++ src/test/java/de/drbunsen/common/fluentxmlwrapper/FluentXmlWrapperTest.java
@@ -0,0 +1,116 @@
+package de.drbunsen.common.fluentxmlwrapper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bkuhlen on 02.03.17.
+ */
+public class FluentXmlWrapperTest {
+    @Test
+    public void addElement() throws Exception {
+
+    }
+
+    @Test
+    public void getElement() throws Exception {
+
+    }
+
+    @Test
+    public void getElements() throws Exception {
+
+    }
+
+    @Test
+    public void hasElement() throws Exception {
+
+    }
+
+    @Test
+    public void removeElement() throws Exception {
+
+    }
+
+    @Test
+    public void getElementCount() throws Exception {
+
+    }
+
+    @Test
+    public void getAllElements() throws Exception {
+
+    }
+
+    @Test
+    public void hasAttribute() throws Exception {
+
+    }
+
+    @Test
+    public void getAttribute() throws Exception {
+
+    }
+
+    @Test
+    public void setAttribute() throws Exception {
+
+    }
+
+    @Test
+    public void removeAttribute() throws Exception {
+
+    }
+
+    @Test
+    public void getText() throws Exception {
+
+    }
+
+    @Test
+    public void setText() throws Exception {
+
+    }
+
+    @Test
+    public void getParentElement() throws Exception {
+
+    }
+
+    @Test
+    public void getRootElement() throws Exception {
+
+    }
+
+    @Test
+    public void getW3cElement() throws Exception {
+
+    }
+
+    @Test
+    public void toXmlWithDefaultUtf8Header() throws Exception {
+
+    }
+
+    @Test
+    public void toXmlWithoutHeader() throws Exception {
+
+    }
+
+    @Test
+    public void of() throws Exception {
+
+    }
+
+    @Test
+    public void of1() throws Exception {
+
+    }
+
+    @Test
+    public void ofNewRootElement() throws Exception {
+
+    }
+
+}
